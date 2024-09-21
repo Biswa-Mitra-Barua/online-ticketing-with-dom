20c278b7a41747dc035bb2010d3d6891bd1ab827
Biswa Mitra Barua <biswamitra960@gmail.com>
2024-09-22 00:16:54 +0600

    completed with the requirements and modal

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const inputCouponEl = document.getElementById('input-coupon-field');
 const couponBtn = document.getElementById('coupon-btn');
 const defaultNoSeatTextEl = document.getElementById('default-no-seat-text');
 const grandTotalEl = document.getElementById('grand-total');
+const phoneNumberEl = document.getElementById('phone-number');
+const nextBtnEl = document.getElementById('next-btn');
 
 let selectedSeat = [];
 let totalPrice = 0;
@@ -66,8 +68,29 @@ document.getElementById('coupon-btn').addEventListener('click',function(){
     else if(couponInputValue === 'Coupon 20'){
         couponSave = totalPrice * 0.20;
     }
+
+    const showCouponPriceEl = document.getElementById('show-coupon-price');
+    showCouponPriceEl.innerHTML = `
+            <p>Discount</p>
+            <p>
+                <span>-BDT:</span>
+                <span>${couponSave.toFixed(2)}</span>
+            </p>`
+
     
     const grandTotalValue = totalPrice - couponSave;
     grandTotalEl.innerText = grandTotalValue.toFixed(2);
     
+})
+
+
+phoneNumberEl.addEventListener('input',function(e){
+        const inputValue = e.target.value;
+        if (inputValue.length >= 11){
+            nextBtnEl.removeAttribute('disabled');
+        }
+})
+
+document.getElementById('continue-btn').addEventListener('click', function(){
+    window.location.reload();
 })
